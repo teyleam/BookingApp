0d30b1e73db96f7e5985ab1a0b70a3b1b6db2e03
TrungLam <huynhtrunglam167@gmail.com>
2024-02-18 20:01:31 +0700

    update MainActivity

--- app/src/main/java/com/aplication/bookingapp/Activity/MainActivity.java
+++ app/src/main/java/com/aplication/bookingapp/Activity/MainActivity.java
@@ -1,16 +1,32 @@
 package com.aplication.bookingapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.aplication.bookingapp.R;
+import com.aplication.bookingapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    private RecyclerView.Adapter adapterRecommended, adapterNearby;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initLocation();
+    }
+
+    private void initLocation() {
+        String [] items = new String[] {"VietNam , VN","NewYork, USA"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        binding.locationSpin.setAdapter(adapter);
     }
 }
