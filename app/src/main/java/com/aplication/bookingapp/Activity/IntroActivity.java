45fb977df10c3c77330a87dc6dfeed9c0d736c92
TrungLam <huynhtrunglam167@gmail.com>
2024-02-18 19:32:58 +0700

    new PropertyDomain

--- app/src/main/java/com/aplication/bookingapp/Activity/IntroActivity.java
+++ app/src/main/java/com/aplication/bookingapp/Activity/IntroActivity.java
@@ -1,14 +1,16 @@
-package com.aplication.bookingapp;
+package com.aplication.bookingapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.aplication.bookingapp.R;
+
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 }
---
+++ app/src/main/java/com/aplication/bookingapp/Domain/PropertyDomain.java
@@ -0,0 +1,6 @@
+package com.aplication.bookingapp.Domain;
+
+import java.io.Serializable;
+
+public class PropertyDomain implements Serializable {
+}
