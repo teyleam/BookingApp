0e0b8b5e4ab7b49c4a69cf1f5446d0b1618754d7
TrungLam <huynhtrunglam167@gmail.com>
2024-02-19 19:23:13 +0700

    update Main

--- app/src/main/java/com/aplication/bookingapp/Activity/MainActivity.java
+++ app/src/main/java/com/aplication/bookingapp/Activity/MainActivity.java
@@ -19,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         initLocation();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
     }
 
     private void initLocation() {
