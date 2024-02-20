0c629b654b256e45088c515bd3f021f1ad050dc7
TrungLam <huynhtrunglam167@gmail.com>
2024-02-20 17:02:53 +0700

    update Detail

--- app/src/main/java/com/aplication/bookingapp/Activity/DetailActivity.java
+++ app/src/main/java/com/aplication/bookingapp/Activity/DetailActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.aplication.bookingapp.Domain.PropertyDomain;
 import com.aplication.bookingapp.R;
 import com.aplication.bookingapp.databinding.ActivityDetailBinding;
+import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
     ActivityDetailBinding binding;
@@ -29,9 +30,30 @@ public class DetailActivity extends AppCompatActivity {
                 finish();
             }
         });
-        int drawableResource = getResources().getIdentifier(object.getPicPath(),"drawable", DetailActivity.this.getPackageName());
+        int drawableResourceID = getResources().getIdentifier(object.getPicPath(),"drawable", DetailActivity.this.getPackageName());
+
+        Glide.with(DetailActivity.this)
+                .load(drawableResourceID)
+                .into(binding.picDetail);
+
+
+        binding.titleTxt.setText(object.getTitle());
+        binding.typeTxt.setText(object.getType());
+        binding.addressTxt.setText(object.getAddress());
+        binding.descriptionTxt.setText(object.getDescription());
+        binding.scoreTxt.setText(""+object.getScore());
+        binding.priceTxt.setText(""+object.getPrice());
+        binding.bedTxt.setText(object.getBed()+" Bed");
+        binding.bathTxt.setText(object.getBath()+"Bath");
+
+        if(object.isWifi()){
+            binding.wifiTxt.setText("Wifi");
+        } else {
+            binding.wifiTxt.setText("No Wifi");
+        }
     }
 
     private void getBundles() {
+        object = (PropertyDomain) getIntent().getSerializableExtra("object");
     }
 }
