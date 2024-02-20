653cb8b7fa62817416d9f4232d9c40c3feeefda0
TrungLam <huynhtrunglam167@gmail.com>
2024-02-20 16:54:24 +0700

    new Detail

--- app/src/main/java/com/aplication/bookingapp/Activity/DetailActivity.java
+++ app/src/main/java/com/aplication/bookingapp/Activity/DetailActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.aplication.bookingapp.Domain.PropertyDomain;
 import com.aplication.bookingapp.R;
+import com.aplication.bookingapp.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
-
+    ActivityDetailBinding binding;
+    private PropertyDomain object;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        getBundles();
+        setVariable();
+    }
+
+    private void setVariable() {
+    }
+
+    private void getBundles() {
     }
 }
